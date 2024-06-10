6058fa7062fa809a59da37958973505c8d8aa40f
Vinzzztty <kevinarnandes21@gmail.com>
2024-06-10 21:05:37 +0700

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ router.get(
     "/google/callback",
     passport.authenticate("google", { failureRedirect: "/login" }),
     (req, res) => {
-        res.redirect("/");
+        res.redirect("https://www.jalanku.xyz/");
     }
 );
 
